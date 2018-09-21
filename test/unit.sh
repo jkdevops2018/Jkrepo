ac0bf4385203795e0fbe939c7a80d08eee61cac5
Cloud User <centos@ip-172-31-85-66.ec2.internal>
2018-09-21 10:24:44 +0000

    Added unit test file

---
+++ test/unit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# test1
+
+if [[ -z "$1" ]] 
+  then
+   echo "Passed string file1 can't be empty"
+   exit 1;
+fi
+
+# test2
+
+if [[ -z "$2" ]]
+  then
+   echo "Passed string file2 can't be empty"
+   exit 1;
+fi
+
+
+
+
+
+
